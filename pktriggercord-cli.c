bf51aebee979a3c6a222d2d046a37340c1fc61fd
asalamon <andras.salamon@melda.info>
2011-01-23 17:04:33 +0000

    writing flash exposure compensation

--- pktriggercord-cli.c
+++ pktriggercord-cli.c
@@ -391,7 +391,7 @@ int main(int argc, char **argv) {
     
 #ifdef DEBUG
 // 0x09
-//pslr_test( camhandle, false, 0x1a, 1, 1,0,0);
+//pslr_test( camhandle, true, 0x1a, 3, -7,10,7);
 #endif
 
     if( status_hex_info || status_info ) {
