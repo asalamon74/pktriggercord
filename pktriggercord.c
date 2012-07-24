b9085c011d6478c6ffb16755ee83c9341e4bc7f9
asalamon <asalamon@caa34438-bbad-49d5-973f-1fb81c1a5d33>
2012-07-24 08:18:39 +0000

    bugfix and 0.77.12 deploy

--- pktriggercord.c
+++ pktriggercord.c
@@ -705,7 +705,7 @@ static gboolean status_poll(gpointer data)
     status_poll_inhibit = true;
 
     if (!camhandle) {
-        camhandle = pslr_init( NULL );
+        camhandle = pslr_init( NULL, NULL );
         if (camhandle) {
             /* Try to reconnect */
             gtk_statusbar_pop(statusbar, sbar_connect_ctx);
