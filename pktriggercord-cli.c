4aefa49837c21ba80b6064979c8841da9c903316
asalamon <asalamon@caa34438-bbad-49d5-973f-1fb81c1a5d33>
2014-03-17 17:36:51 +0000

    identify camera by swapped id

--- pktriggercord-cli.c
+++ pktriggercord-cli.c
@@ -501,7 +501,7 @@ int main(int argc, char **argv) {
 
     gettimeofday(&prev_time, NULL);
     while (!(camhandle = pslr_init( model, device ))) {
-      DPRINT("foundinwhile\n");
+        DPRINT("foundinwhile\n");
         gettimeofday(&current_time, NULL);
 	if( timeout == 0 || timeout > timeval_diff(&current_time, &prev_time) / 1000000.0 ) {
 	  sleep_sec(1);
