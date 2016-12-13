3347d4ddea1ceb6653baa6e56c5caa3f9e6d002a
Andras Salamon <andras.salamon@melda.info>
2016-12-13 18:08:38 +0100

    Testing K-70

--- pslr_model.c
+++ pslr_model.c
@@ -659,6 +659,7 @@ ipslr_model_info_t camera_models[] = {
     { 0x1301a, "K-S1",        0, 1, 1, 452,  3, {20, 12, 6, 2}, 9, 6000, 100, 51200, 100, 51200, PSLR_JPEG_IMAGE_TONE_BLEACH_BYPASS, 1, ipslr_status_parse_k3    },
     { 0x13024, "K-S2",        0, 1, 1, 452,  3, {20, 12, 6, 2}, 9, 6000, 100, 51200, 100, 51200, PSLR_JPEG_IMAGE_TONE_BLEACH_BYPASS, 1, ipslr_status_parse_k3    },
     { 0x13092, "K-1",         0, 1, 1, 456,  3, {36, 22, 12, 2}, 9, 8000, 100, 204800, 100, 204800, PSLR_JPEG_IMAGE_TONE_RADIANT, 1, ipslr_status_parse_k1      },	
+    { 0x13222, "K-70",        0, 1, 1, 0,   3, {24, 14, 6, 2}, 9, 6000, 100, 102400, 100, 102400, PSLR_JPEG_IMAGE_TONE_RADIANT, 1, NULL }
 };
 
 ipslr_model_info_t *find_model_by_id( uint32_t id ) {
