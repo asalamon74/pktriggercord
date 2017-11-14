c4c0930cd5fdb489e028b5f2fcf5e773e2b724b8
Andras Salamon <andras.salamon@melda.info>
2017-11-14 15:21:58 +0000

    adding json lib to Android.mk

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -12,6 +12,6 @@ LOCAL_SRC_FILES := ../../pslr_enum.c \
 	../../pktriggercord-cli.c
 DEFINES 	:= -DANDROID -DVERSION=\"$(VERSION)\" 
 LOCAL_CFLAGS  	:= $(DEFINES) -frtti -I.. -Istlport -g -fPIE
-LOCAL_LDLIBS	:= -llog -lstdc++ -fPIE -pie
+LOCAL_LDLIBS	:= -llog -lstdc++ -ljson-c -fPIE -pie
 
 include $(BUILD_EXECUTABLE)
