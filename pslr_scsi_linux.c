asalamon74/pktriggercord

commit f1d6645d34a6c1f9d35fd5d638f0f1515ca22d12

refactor get_drive_info method in pslr_scsi_linux

pslr_scsi_linux.c
@@ -101,31 +101,32 @@ char **get_drives(int *driveNum) {
     return ret;
 }
 
-pslr_result get_drive_info(char* driveName, int* hDevice,
-                           char* vendorId, int vendorIdSizeMax,
-                           char* productId, int productIdSizeMax) {
+pslr_result get_drive_info_vendor(const char *driveName, char *vendorId, int vendorIdSizeMax) {
     char nmbuf[256];
     int fd;
 
-    DPRINT("Getting drive info for %s\n", driveName);
-    vendorId[0] = '\0';
-    productId[0] = '\0';
     DPRINT("Looking for vendor\n");
-    snprintf(nmbuf, sizeof (nmbuf), "/sys/class/scsi_generic/%s/device/vendor", driveName);
+    snprintf(nmbuf, sizeof(nmbuf), "/sys/class/scsi_generic/%s/device/vendor", driveName);
     fd = open(nmbuf, O_RDONLY);
     if (fd == -1) {
         DPRINT("Cannot open /sys/class/scsi_generic/%s/device/vendor\n", driveName);
-        snprintf(nmbuf, sizeof (nmbuf), "/sys/block/%s/device/vendor", driveName);
+        snprintf(nmbuf, sizeof(nmbuf), "/sys/block/%s/device/vendor", driveName);
         fd = open(nmbuf, O_RDONLY);
         if (fd == -1) {
             DPRINT("Cannot open /sys/block/%s/device/vendor\n", driveName);
             return PSLR_DEVICE_ERROR;
         }
     }
-    int v_length = read(fd, vendorId, vendorIdSizeMax-1);
-    vendorId[v_length]='\0';
+    int v_length = read(fd, vendorId, vendorIdSizeMax - 1);
+    vendorId[v_length] = '\0';
     DPRINT("vendorId: %s\n", vendorId);
     close(fd);
+    return PSLR_OK;
+}
+
+pslr_result get_drive_info_model(const char *driveName, char *productId, int productIdSizeMax) {
+    char nmbuf[256];
+    int fd;
 
     DPRINT("Looking for model\n");
     snprintf(nmbuf, sizeof (nmbuf), "/sys/class/scsi_generic/%s/device/model", driveName);
@@ -143,6 +144,11 @@ pslr_result get_drive_info(char* driveName, int* hDevice,
     productId[p_length]='\0';
     DPRINT("product id: %s\n", productId);
     close(fd);
+    return PSLR_OK;
+}
+
+pslr_result get_drive_info_device(const char *driveName, int* hDevice) {
+    char nmbuf[256];
 
     DPRINT("Looking for device file\n");
     snprintf(nmbuf, sizeof (nmbuf), "/dev/%s", driveName);
@@ -156,6 +162,18 @@ pslr_result get_drive_info(char* driveName, int* hDevice,
             return PSLR_DEVICE_ERROR;
         }
     }
+    return PSLR_OK;
+}
+
+pslr_result get_drive_info(char* driveName, int* hDevice,
+                           char* vendorId, int vendorIdSizeMax,
+                           char* productId, int productIdSizeMax) {
+    DPRINT("Getting drive info for %s\n", driveName);
+    vendorId[0] = '\0';
+    productId[0] = '\0';
+    CHECK(get_drive_info_vendor(driveName, vendorId, vendorIdSizeMax));
+    CHECK(get_drive_info_model(driveName, productId, productIdSizeMax));
+    CHECK(get_drive_info_device(driveName, hDevice));
     return PSLR_OK;
 }
 

pslr.c
@@ -57,15 +57,6 @@
 #define BLOCK_RETRY 3 /* Number of retries, since we can occasionally
                        * get SCSI errors when downloading data */
 
-#define CHECK(x) do {                           \
-        int __r;                                \
-        __r = (x);                                                      \
-        if (__r != PSLR_OK) {                                           \
-            fprintf(stderr, "%s:%d:%s failed: %d\n", __FILE__, __LINE__, #x, __r); \
-            return __r;                                                 \
-        }                                                               \
-    } while (0)
-
 void sleep_sec(double sec) {
     int i;
     for (i=0; i<floor(sec); ++i) {

pslr_model.c
@@ -926,7 +926,6 @@ void ipslr_settings_parser_json(const char *cameraid, ipslr_handle_t *p, pslr_se
         } else if (strcmp(defs[def_index].name, "horizon_correction")==0) {
             settings->horizon_correction = bool_setting;
         }
-
         ++def_index;
     }
 }

pslr_scsi.h
@@ -47,6 +47,15 @@ extern void write_debug( const char* message, ... );
 #endif
 #endif
 
+#define CHECK(x) do {                           \
+        int __r;                                \
+        __r = (x);                                                      \
+        if (__r != PSLR_OK) {                                           \
+            fprintf(stderr, "%s:%d:%s failed: %d\n", __FILE__, __LINE__, #x, __r); \
+            return __r;                                                 \
+        }                                                               \
+    } while (0)
+
 typedef enum {
     PSLR_OK = 0,
     PSLR_DEVICE_ERROR,

commit 4d0e34750fd0c39c0aea473d28240de41ba5e685

Fixing drive info reading bug
Bug was introduced by commit: f1d6645d34a6c1f9d35fd5d638f0f1515ca22d12

pslr_scsi_linux.c
@@ -140,7 +140,7 @@ pslr_result get_drive_info_model(const char *drive_name, char *product_id, int p
 pslr_result get_drive_info_device(const char *drive_name, int* device) {
     char file_name[256];
 
-    DPRINT("Looking for device file\n");
+    DPRINT("Looking for device file %s\n", drive_name);
     snprintf(file_name, sizeof (file_name), "/dev/%s", drive_name);
     *device = open(file_name, O_RDWR);
     if ( *device == -1) {
@@ -161,10 +161,15 @@ pslr_result get_drive_info(char* drive_name, int* device,
     DPRINT("Getting drive info for %s\n", drive_name);
     vendor_id[0] = '\0';
     product_id[0] = '\0';
-    CHECK(get_drive_info_vendor(drive_name, vendor_id, vendor_id_size_max));
-    CHECK(get_drive_info_model(drive_name, product_id, product_id_size_max));
-    CHECK(get_drive_info_device(drive_name, device));
-    return PSLR_OK;
+    pslr_result result;
+    result = get_drive_info_vendor(drive_name, vendor_id, vendor_id_size_max);
+    if (result == PSLR_OK) {
+        result = get_drive_info_model(drive_name, product_id, product_id_size_max);
+    }
+    if (result == PSLR_OK) {
+        result = get_drive_info_device(drive_name, device);
+    }
+    return result;
 }
 
 void close_drive(int *device) {